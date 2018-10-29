4ad199b1761970337f4d626da43e056f5f67b5f2
Donna <donnaagius1995@gmail.com>
2018-10-29 18:39:51 +0100

    added docker

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,10 @@
-insert into Account
-values(111,200);
-insert into Account
-values(222,300);
+insert into account
+values(400123456,100.50);
+insert into account
+values(400234567,200);
+insert into account
+values(400345678,10000);
+insert into account
+values(400456789,64);
+insert into account
+values(400567890,32);
